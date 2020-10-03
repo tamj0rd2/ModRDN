4c5a8605a40ce53949a3c5748e301f7c9266f93f
tamj0rd2 <tamara.jordan1+coding@hotmail.com>
2020-10-03 12:51:15 +0100

    Add additional args to ebp extractor

--- ebp-extractor.py
+++ ebp-extractor.py
@@ -27,28 +27,57 @@ def getEbpControllerType(filePath):
                 return Result(filePath, controllerType)
 
 
-def getEbpControllerTypeResults(searchDir, allowZero, orderBy):
-    os.chdir(searchDir[0])
+def filenameMatchesFilter(filename, matchStr):
+    if matchStr is None:
+        return True
+    return matchStr in filename
 
+
+def typeMatchesFilter(type, matchType, allowZero):
+    if type == 0:
+        return allowZero or matchType == 0
+
+    if matchType is not None:
+        return type == matchType
+
+    return True
+
+
+def getEbpControllerTypeResults(searchDir, allowZero, orderBy, matchFilename, matchType):
+    os.chdir(searchDir)
     results = [getEbpControllerType(file)
-               for file in glob.glob("**/*.ebp", recursive=True)]
+               for file in glob.glob("**/*.ebp", recursive=True) if filenameMatchesFilter(file, matchFilename)]
     sortedResults = sorted(filter(None.__ne__, results), key=lambda result: result.controllerType if orderBy ==
                            "controllerType" else result.filePath)
 
     for result in sortedResults:
-        if (allowZero or result.controllerType > 0):
+        if typeMatchesFilter(result.controllerType, matchType, allowZero):
             print("{}\t{}".format(result.controllerType, result.filePath))
 
 
 def parseArgs():
+    icDevDirEnvName = "IC_DEV_DIR"
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("searchDir", nargs=1,
-                        help="A folder to search for EBPs within")
+    parser.add_argument("--searchDir", type=str,
+                        help="A folder to search for EBPs within", default=os.environ.get(icDevDirEnvName))
     parser.add_argument("--allowZero", action="store_true",
                         help="Show files whose controller type is 0")
     parser.add_argument(
         "--orderBy", choices=["controllerType", "fileName"], default="fileName", help="Choose ordering of results")
+    parser.add_argument(
+        "--matchFile", help="only show results if the filename matches the given string", dest="matchFilename")
+    parser.add_argument("--matchType",
+                        help="only show results if the controller type matches the given number", type=int)
     args = parser.parse_args()
+
+    if args.__dict__.get("searchDir") is None:
+        parser.print_help()
+        parser.error(
+            "You must provide a searchDir or set the {} environment variable to the location where the game is installed".format(
+                icDevDirEnvName)
+        )
+
     return args.__dict__
 
 
