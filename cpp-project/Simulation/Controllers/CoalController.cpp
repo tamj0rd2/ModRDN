e358df195faf3180b8b17ce90ce45b4e6592c4af
tamj0rd2 <tamara.jordan1+coding@hotmail.com>
2020-10-10 10:34:52 +0100

    Add resource ext to coal controller

--- cpp-project/Simulation/Controllers/CoalController.cpp
+++ cpp-project/Simulation/Controllers/CoalController.cpp
@@ -24,23 +24,29 @@
 CoalController::StaticInfo::StaticInfo(const ControllerBlueprint *cbp)
 		: UIExtInfo(cbp),
 			SiteExtInfo(cbp),
-			ModStaticInfo(cbp)
+			ModStaticInfo(cbp),
+			ResourceExtInfo(cbp)
 {
 }
 
 const ModStaticInfo::ExtInfo *
 CoalController::StaticInfo::QInfo(unsigned char id) const
 {
-	if (id == SiteExtInfo ::ExtensionID)
+	if (id == SiteExtInfo::ExtensionID)
 		return static_cast<const SiteExtInfo *>(this);
+
 	if (id == UIExtInfo::ExtensionID)
 		return static_cast<const UIExtInfo *>(this);
+
+	if (id == ResourceExtInfo::ExtensionID)
+		return static_cast<const ResourceExtInfo *>(this);
 
 	return 0;
 }
 
 CoalController::CoalController(Entity *pEntity, const ECStaticInfo *pStaticInfo)
-		: ModController(pEntity, new GroundDynamics(static_cast<SimEntity *>(pEntity)), pStaticInfo)
+		: ModController(pEntity, new GroundDynamics(static_cast<SimEntity *>(pEntity)), pStaticInfo),
+			ResourceExt()
 
 {
 	// init the Command Processor
@@ -56,4 +62,26 @@ CoalController::~CoalController()
 ModController *CoalController::GetSelf()
 {
 	return this;
+}
+
+float CoalController::GetResources() const
+{
+	return 0;
+}
+
+void CoalController::SetResources(float amount)
+{
+}
+
+float CoalController::DecResources(float amount)
+{
+	return 0;
+}
+
+void CoalController::OnZeroResources()
+{
+}
+
+void CoalController::OnResourceProgress(float amount)
+{
 }
--- cpp-project/Simulation/Controllers/CoalController.h
+++ cpp-project/Simulation/Controllers/CoalController.h
@@ -3,18 +3,19 @@
 #include "ModController.h"
 
 #include "../Extensions/ResourceExt.h"
-#include "../Extensions/ResourceExt.h"
 
 #include "../ExtInfo/SiteExtInfo.h"
 #include "../ExtInfo/ResourceExtInfo.h"
 #include "../ExtInfo/UIExtInfo.h"
 
-class CoalController : public ModController
+class CoalController : public ModController,
+											 private ResourceExt
 {
 	// types
 public:
 	class StaticInfo : private UIExtInfo,
 										 private SiteExtInfo,
+										 private ResourceExtInfo,
 										 public ModStaticInfo
 	{
 	public:
@@ -28,6 +29,20 @@ public:
 public:
 	CoalController(Entity *pEntity, const ECStaticInfo *);
 	virtual ~CoalController();
+
+	// inherited from ResourceExt.h
+public:
+	virtual float GetResources() const;
+	virtual void SetResources(float amount);
+
+	// return amount decreased
+	virtual float DecResources(float amount);
+
+protected:
+	virtual void OnZeroResources();
+
+private:
+	virtual void OnResourceProgress(float amount);
 
 private:
 	virtual ModController *GetSelf();
